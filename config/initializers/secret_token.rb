aae1af71e1ff179ad50c8c227ef3be769eca79bd
Robby Ranshous <rranshous@gmail.com>
2014-03-05 13:37:10 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pushto::Application.config.secret_key_base = 'd41cdddfc7496c0b4c9f229f620b0f97cc42d118dcf3a0f4314d67ee230ab52df0f85190103ae3ac362ef9b9b9ded50fa8e78fca63441d06d37221cf6b643a87'
